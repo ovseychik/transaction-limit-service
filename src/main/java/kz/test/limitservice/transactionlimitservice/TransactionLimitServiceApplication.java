aaff81fd4b8a333626b45d82cfcc43cd2a24bc03
ovseychik <github@ovseychik.me>
2024-04-23 21:35:04 +0500

    Initial commit

---
+++ src/main/java/kz/test/limitservice/transactionlimitservice/TransactionLimitServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.test.limitservice.transactionlimitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLimitServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionLimitServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kz/test/limitservice/transactionlimitservice/TransactionLimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.test.limitservice.transactionlimitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLimitServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
